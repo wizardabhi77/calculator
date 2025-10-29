ead146f6a2364bc0a6899ce4a373091b52fd7f11
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-10-29 12:02:50 +0530

    Add 4 basic math functions
    Addition,subtraction,multiplication,divison

---
+++ calc.js
@@ -0,0 +1,18 @@
+
+function add (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract (firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function mutliply (firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide (firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+console.log(divide(2,2));
